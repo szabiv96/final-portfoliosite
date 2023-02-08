214ad9db801e4f7e1bdcf8322e2a6e0563d5ed26
szabiv96 <szabi.v96@gmail.com>
2023-02-08 21:41:48 +0100

    upd

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -17,12 +17,7 @@ export default function Introduction({homeRef, introRef}) {
                         <div><h2>... and the wheel is already invented, what is needs to be perfected!</h2></div>
                     </Col>
                     <Col className='d-flex justify-content-between flex-row '>
-                        <div className='mr w-100'><p>As a junior web developer, it is really 
-                            important for me to seize every opportunity to develop my skills and also myself, so I am constantly working on something. During the
-                             university, I gained different kind of work experiences, and improved my visual, aesthetic and design skillset. As part of Codecool, I learned the basics of coding.
-                              Now I am confidently using different graphic design softwares for supporting my work as a front end developer. 
-                              My different kind of experiences are prepeared me working with different people with different professional background and 
-                              understand my role in a complex work enviroment.</p></div>
+                        <div className='mr w-100'><p>As a junior web developer, it is really important for me to seize every opportunity to develop my skills and also myself, so I am constantly working on something. During university, I gained different kinds of work experiences, and improved my visual, aesthetic and design skill set. As part of Codecool, I learned the basics of coding. Now I am confidently using different graphic design softwares for supporting my work as a front end developer. My different kinds of experiences have prepared me to work with different people with different professional backgrounds and understand my role in a complex work environment.</p></div>
                         <div ><img src={self} alt="" width="100%" /></div>
                     </Col>
                 </Row>
